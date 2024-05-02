804b4b5e9a9f58289f956dfff75c1973d9d42ea2
sathishss08 <sathishgd95@hotmail.com>
2024-05-02 11:51:11 +0530

    added homepage

---
+++ src/main/java/com/qa/opencart/pages/Homepage.java
@@ -0,0 +1,27 @@
+package com.qa.opencart.pages;
+
+public class Homepage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int i = 10;
+		
+		int j = 20;
+		
+		if (i>j)
+		{
+		System.out.println("this is true");	
+		
+		}
+		else {
+			
+			System.out.println("this is not true");
+	}
+		
+		}
+
+	}
+
+	
