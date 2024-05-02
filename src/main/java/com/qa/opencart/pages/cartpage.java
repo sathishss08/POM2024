c8897fdeca01f73d58faa8af27be16dc7633696d
sathishss08 <sathishgd95@hotmail.com>
2024-05-02 11:39:09 +0530

    added homepage

--- src/main/java/com/qa/opencart/pages/cartpage.java
+++ src/main/java/com/qa/opencart/pages/cartpage.java
@@ -8,7 +8,30 @@ public class cartpage {
 		
 		
 		String productname = "iphone";
+		
+		
+		
+		int len = productname.length();
+		String rev =" ";
+		
+		for (int i=len-1;i>=0;i--)
+		{
+			
+			rev = rev+productname.charAt(i);
+		}
+			
+			
+			System.out.println(rev);
+			
+			
+		}
+		
+		
+		
+		
+		
+		
 
 	}
 
-}
+
